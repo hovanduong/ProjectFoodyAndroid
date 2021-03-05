db5fcff6133787d1d621cf881a8cf10be748f04f
hovanduong <52810802+hovanduong@users.noreply.github.com>
2021-03-05 10:23:37 +0700

    update mới nhất

---
+++ app/src/main/java/com/example/foody2/Controller/interfaces/ThucDonInterface.java
@@ -0,0 +1,14 @@
+package com.example.foody2.Controller.interfaces;
+
+import com.example.foody2.Model.ThucDonModel;
+
+
+import java.util.List;
+
+/**
+ * Created by Binh on 7/10/17.
+ */
+
+public interface ThucDonInterface {
+    public void getThucDonThanhCong(List<ThucDonModel> thucDonModelList);
+}
---
+++ app/src/main/java/com/example/foody2/View/GiohangActivity.java
@@ -0,0 +1,31 @@
+package com.example.foody2.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.foody2.R;
+
+public class GiohangActivity extends AppCompatActivity {
+    TextView txtTenmonanngiohang,txtgiamonangiohang;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giohang);
+
+        txtTenmonanngiohang=findViewById(R.id.txtTenmonanngiohang);
+        txtgiamonangiohang=findViewById(R.id.txtgiamonangiohang);
+
+        Intent intent = getIntent();
+        String tenmonan = intent.getStringExtra("tenmonan");
+        String giamonan = intent.getStringExtra("giamonan");
+
+        txtgiamonangiohang.setText(giamonan );
+        txtTenmonanngiohang.setText(tenmonan);
+
+
+    }
+}

fdc0eb01330580bd0b4e8c4d405c048afa0e8482
hovanduong <52810802+hovanduong@users.noreply.github.com>
2021-03-13 19:16:16 +0700

    update themquanan

---
+++ app/src/main/java/com/example/foody2/View/ThemThucDonModel.java
@@ -0,0 +1,32 @@
+package com.example.foody2.View;
+
+import com.example.foody2.Model.MonAnModel;
+
+/**
+ * Created by Binh on 8/18/17.
+ */
+
+public class ThemThucDonModel {
+    String mathucdon;
+    MonAnModel monAnModel;
+
+    public MonAnModel getMonAnModel() {
+        return monAnModel;
+    }
+
+    public void setMonAnModel(MonAnModel monAnModel) {
+        this.monAnModel = monAnModel;
+    }
+
+
+
+    public String getMathucdon() {
+        return mathucdon;
+    }
+
+    public void setMathucdon(String mathucdon) {
+        this.mathucdon = mathucdon;
+    }
+
+
+}
